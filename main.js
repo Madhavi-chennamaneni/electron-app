581e1c19eb893e59a5af8efa7f068548087de5f0
Mathanleo <mathan.u@gradious.com>
2022-07-21 15:04:34 +0530

    gogle auth profile updates

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
 //const jwt_decode = require('jwt-decode');
 
-function createWindow () {
+function createWindow() 
+{
+
   // Create the browser window.
+
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
@@ -13,25 +18,32 @@ function createWindow () {
     }
   })
 
+  mainWindow.webContents.openDevTools();
+
   // and load the index.html of the app.
+
   mainWindow.loadFile('index.html')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 
   //Remove menu
+
   mainWindow.removeMenu();
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
+
 app.whenReady().then(() => {
   createWindow()
 
   app.on('activate', function () {
+
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
+
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
@@ -39,6 +51,7 @@ app.whenReady().then(() => {
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
+
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
